27f3fd93a4f4a33aacf80e82e8af6b8d3c0e72bf
ya_vyachko <konkursirus@gmail.com>
2024-10-25 17:43:33 +0300

    idk

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="wrapper">
+        <div className="left">
+          <span>LAOLAB CYBER SECURITY LLC</span>
+          <span>License CN-5076043</span>
+        </div>
+        <div className="right">
+          Al Danah E5, Dar Al Salam building, office #619 Abu Dhabi
+        </div>
+      </div>
+    </footer>
+  )
+}
